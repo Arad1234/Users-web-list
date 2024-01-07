be1fb6c17302076ca1a49431af09fda1b78abfcc
unknown <Arad1234>
2024-01-07 11:53:44 +0200

    Deleted generateToken modules

--- server/src/utils/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-import config from './config';
-
-export const generateToken = (payload: { email: string }) => {
-	const token = jwt.sign(payload, config.auth.secretKey!);
-
-	return token;
-};
